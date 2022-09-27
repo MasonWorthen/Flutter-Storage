a315c6135ab8fca0fb0d81e23a4979e79cf668fd
Mason C. Worthen <98508225+worthenmc@users.noreply.github.com>
2022-09-27 13:16:21 -0500

    Add files via upload

---
+++ storage.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class Storage {
+  final storage = const FlutterSecureStorage();
+
+  start(List<String> keys) async {
+    try {
+     for(int r=0; r<keys.length;r++){
+      await storage.read(key: keys[r])
+      await storage.write(key: keys[r], value: '');
+    }
+    } 
+    catch (e) {
+      return '';
+    }
+  }
+
+  Future<bool> write(String name, String value) async {
+    try {
+      await storage.write(key: name, value: token).then((value) async => {
+        
+          });
+      return true;
+    } 
+    catch (e) {
+      return false;
+    }
+  }
+
+  Future<String> read(String name) async {
+     try {
+      String? data = await storage.read(key: name);
+      return data ?? '';
+    } 
+    catch (e) {
+      return '';
+    }
+   
+  }
+}
